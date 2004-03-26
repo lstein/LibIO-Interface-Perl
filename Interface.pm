049d4fa8919c853fc58c62e365b4de144a3ddda0
lstein <lstein>
2004-03-26 23:21:01 +0000

    Fixed minor documentation error.

--- Interface.pm
+++ Interface.pm
@@ -25,7 +25,7 @@ my @flags     = qw(IFF_ALLMULTI    IFF_AUTOMEDIA  IFF_BROADCAST
 @EXPORT = qw( );
 
 @ISA = qw(Exporter DynaLoader);
-$VERSION = '0.97';
+$VERSION = '0.98';
 
 sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
