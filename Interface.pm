b589b4b28d8dcaba4b44fbef2e0988fc3121b0f9
Lincoln Stein <lincoln.stein@gmail.com>
2014-12-08 10:27:32 -0500

    add copyright statement

--- Interface.pm
+++ Interface.pm
@@ -285,9 +285,32 @@ This example determines whether interface 'tu0' supports multicasting:
 
 =head1 AUTHOR
 
-Lincoln Stein E<lt>lstein@cshl.orgE<gt>
+Perl interface to Unix network interface API
 
-This module is distributed under the same license as Perl itself.
+IO::Interface adds object methods to IO::Socket objects to allow them
+|to get and set operational characteristics of network interface
+cards, such as IP addresses, net masks, and so forth.  It is useful
+for identifying runtime characteristics of cards, such as broadcast
+addresses, and finding interfaces that satisfy certain criteria,
+such Perl interface to Unix network interface API as the ability to
+multicast.
+
+For support, please use the GitHub repository at
+https://github.com/lstein/LibIO-Interface-Perl
+
+Author
+======
+
+Lincoln D. Stein <lincoln.stein@gmail.com>
+
+License
+=======
+
+Copyright 2001-2014, Lincoln D. Stein.
+
+This library is distributed under the Perl Artistic License
+2.0. Please see LICENSE for more information.
+
 
 =head1 SEE ALSO
 
