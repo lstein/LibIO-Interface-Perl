f847e24a4309e68cb8f1596f3c1f7bf263cfc02f
Mikolaj Kucharski <mikolaj@kucharski.name>
2017-12-10 16:58:27 +0000

    Fix build flags after switching to Module::Build

--- lib/IO/Interface.pm
+++ lib/IO/Interface.pm
@@ -27,7 +27,7 @@ my @flags     = qw(IFF_ALLMULTI    IFF_AUTOMEDIA  IFF_BROADCAST
 @EXPORT = qw( );
 
 @ISA = qw(Exporter DynaLoader);
-$VERSION = '1.09';
+$VERSION = '1.10';
 
 sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
